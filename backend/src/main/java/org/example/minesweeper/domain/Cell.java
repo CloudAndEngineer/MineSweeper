246e8b7f67cf3b56fd318bf232d9d357320be3b9
CloudAndEngineer <fjdksla6@gmail.com>
2026-09-17 21:23:56 +0900

    Feat: Cell and CellTest

--- backend/src/main/java/org/example/minesweeper/domain/Cell.java
+++ backend/src/main/java/org/example/minesweeper/domain/Cell.java
@@ -1,9 +1,32 @@
 package org.example.minesweeper.domain;
 
 public class Cell {
+    private int numberOfMines;
     private boolean isOpen = false;
+    private boolean flag = false;
+    private boolean mine;
 
     public boolean isOpen() {
-        return isOpen;
+        return this.isOpen;
+    }
+
+    public boolean isFlagged() {
+        return this.flag;
+    }
+
+    public boolean isMine() {
+        return this.mine;
+    }
+
+    public void open() {
+        if(!flag) {
+            isOpen = true;
+        }
+    }
+
+    public void toggleFlag() {
+        if(!isOpen) {
+            flag = !flag;
+        }
     }
 }
