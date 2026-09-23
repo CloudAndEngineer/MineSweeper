fe00d03b2b11f8b40bf33dd044da5d6964e7655c
CloudAndEngineer <fjdksla6@gmail.com>
2026-09-23 20:18:03 +0900

    Feat : BoardResponse

--- backend/src/main/java/org/example/minesweeper/dto/BoardResponse.java
+++ backend/src/main/java/org/example/minesweeper/dto/BoardResponse.java
@@ -1,4 +1,31 @@
 package org.example.minesweeper.dto;
 
-public class BoardResponse {
+import org.example.minesweeper.domain.*;
+
+import java.util.*;
+
+public record BoardResponse (
+        UUID gameId,
+        int width,
+        int height,
+        GameStatus status,
+        List<CellResponse> cells
+) {
+    public static BoardResponse of(UUID gameId, Board board) {
+        List<CellResponse> cellResponses = new ArrayList<>();
+
+        for (int y = 0; y < board.getHeight(); y++) {
+            for (int x = 0; x < board.getWidth(); x++) {
+                cellResponses.add(CellResponse.from(x, y, board.getCell(x, y)));
+            }
+        }
+
+        return new BoardResponse(
+                gameId,
+                board.getWidth(),
+                board.getHeight(),
+                board.getStatus(),
+                cellResponses
+        );
+    }
 }
