e29c21eea0ba319e1db5546f9263108ce78ff4e7
CloudAndEngineer <fjdksla6@gmail.com>
2026-09-26 22:03:43 +0900

    Feat : GameRepository

--- backend/src/main/java/org/example/minesweeper/repository/GameRepository.java
+++ backend/src/main/java/org/example/minesweeper/repository/GameRepository.java
@@ -1,4 +1,30 @@
 package org.example.minesweeper.repository;
 
+import org.example.minesweeper.domain.Board;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
 public class GameRepository {
+    private final Map<UUID, Board> store = new ConcurrentHashMap<>();
+
+    public UUID save(Board board) {
+        UUID gameId = UUID.randomUUID();
+        store.put(gameId, board);
+        return gameId;
+    }
+
+    public Optional<Board> findById(UUID gameId) {
+        return Optional.ofNullable(store.get(gameId));
+    }
+
+    public void deleteById(UUID gameId) {
+        store.remove(gameId);
+    }
+
+    public void clear() {
+        store.clear();
+    }
 }
